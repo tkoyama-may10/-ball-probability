393e79e4445e82ff0dab0dc85374881ecd649b59
tkoyama-may10 <tkoyama041127@gmail.com>
2015-08-20 22:25:28 +0900

    added tab4.sh and tab4.tex

---
+++ tab4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./a.out 10  3 1.0 > tab4.tex
+./a.out 10  4 1.0 >> tab4.tex
+./a.out 10  5 1.0 >> tab4.tex
+./a.out 10  6 1.0 >> tab4.tex
+./a.out 10  7 1.0 >> tab4.tex
+./a.out 10  8 1.0 >> tab4.tex
+./a.out 10  9 1.0 >> tab4.tex
+./a.out 10 10 1.0 >> tab4.tex
